7a1ae58119a8fb9f74d93e31399f018ff18b2b33
Ubuntu <michael.w.oleary@gmail.com>
2020-07-23 14:56:52 +0000

    adding deploymentScriptContent for postDeploymentCheck

---
+++ templates/postDeployCheck/deploymentScriptContent.ps1
@@ -0,0 +1,32 @@
+param(
+    [string] $url
+    )
+
+#every 30 secs for 10 mins, check if app server is up yet
+$i=0
+write-output 'setting i to 0'
+while ($i -lt 20) {
+    try {
+        write-output $('attempt number'+$i)
+        $response = Invoke-WebRequest -URI $url -UseBasicParsing
+        $StatusCode = $response.StatusCode 
+    }
+    catch {
+        $StatusCode = $_.Exception.Response.StatusCode.value__
+    }
+    if ($StatusCode -eq '200') {
+        break;
+    }
+    else {
+        $i++
+        Start-Sleep 30
+    }
+}
+if (!($StatusCode -eq '200')) {
+    $output = $StatusCode
+    throw $url+' is not available, status code is '+$StatusCode
+    }
+$output = $StatusCode
+Write-Output $output
+$DeploymentScriptOutputs = @{}
+$DeploymentScriptOutputs['text'] = $output
